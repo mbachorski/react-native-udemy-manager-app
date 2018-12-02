ebea83bac82a26936c8c32a867d5fa372683f3e9
Marek Bachorski <m.bachorski@gmail.com>
2018-12-02 15:04:47 +0100

    using redux thunk in a hacky way without dispatching action

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,10 +1,32 @@
 import {
     EMPLOYEE_UPDATE
 } from "./types";
+import firebase from '@firebase/app';
+import '@firebase/database';
+import '@firebase/auth';
+import {Actions} from 'react-native-router-flux';
 
 export const employeeUpdate = ({prop, value}) => {
     return {
         type: EMPLOYEE_UPDATE,
         payload: {prop, value}
     }
+};
+
+export const employeeCreate = ({name, phone, shift}) => {
+    console.log(name, phone, shift)
+    const {currentUser} = firebase.auth();
+
+    // breaking the rules -> using redux thunk but not returning a function (dispatch). We just want to navigate nothing more
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .push({name, phone, shift})
+            .then(() => Actions.employeeList());
+            // could be .then(() => Actions.employeeList({type: 'reset'})); instead of popping scene
+    }
+
+    // return {
+    //     type: EMPLOYEE_UPDATE,
+    //     payload: {prop, value}
+    // }
 };
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import {Picker, Text} from 'react-native'
 import {connect} from 'react-redux';
-import {employeeUpdate} from '../actions'
+import {employeeUpdate, employeeCreate} from '../actions'
 import {Button, Card, CardSection, Input} from "./common";
 
 class EmployeeCreate extends Component {
+
+    onButtonPress() {
+        const {name, phone, shift} = this.props;
+        // '' || 'Monday' -> 'Monday'
+        // INITIAL_STATE in reducer is '' for shift
+        this.props.employeeCreate({name, phone, shift: shift || 'Monday'});
+    }
+
     render() {
         return (
             <Card>
@@ -43,7 +51,7 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>Create</Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>Create</Button>
                 </CardSection>
 
             </Card>
@@ -63,4 +71,4 @@ const mapStateToProps = (state) => {
     return {name, phone, shift};
 }
 
-export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate);
+export default connect(mapStateToProps, {employeeUpdate, employeeCreate})(EmployeeCreate);
