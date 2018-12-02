c139df6a5c53162d920ab1f8c9a35f1b750d554d
Marek Bachorski <m.bachorski@gmail.com>
2018-12-02 12:56:50 +0100

    ADDS simple navigation to create employee form

--- src/Router.js
+++ src/Router.js
@@ -2,16 +2,38 @@ import React from 'react';
 import {Router, Scene} from "react-native-router-flux";
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import {Actions} from 'react-native-router-flux';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root" hideNavBar>
                 <Scene key="auth">
-                    <Scene key="login" component={LoginForm} title="Please Login" initial/>
+                    <Scene
+                        key="login"
+                        component={LoginForm}
+                        title="Please Login"
+                        initial
+                    />
                 </Scene>
                 <Scene key="main">
-                    <Scene key="employeeList" component={EmployeeList} title="Employees"/>
+                    <Scene
+                        rightTitle="Add"
+                        onRight={() => {
+                            console.log("onRight");
+                            Actions.employeeCreate();
+                        }}
+                        key="employeeList"
+                        component={EmployeeList}
+                        title="Employees"
+                        initial
+                    />
+                    <Scene
+                        key="employeeCreate"
+                        component={EmployeeCreate}
+                        title="Create Employee"
+                    />
                 </Scene>
             </Scene>
         </Router>
