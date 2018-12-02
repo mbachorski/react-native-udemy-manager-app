2d601e6518f48315738d5334b9761668263be66e
Marek Bachorski <m.bachorski@gmail.com>
2018-12-02 13:07:30 +0100

    ADDS simple create employee form with name, phone and button

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {Button, Card, CardSection, Input} from "./common";
+
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>
-                    Employee Form
-                </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Marek"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        placeholder="111-222-3333"
+                    />
+                </CardSection>
+
+                <CardSection>
+
+                </CardSection>
+
+                <CardSection>
+                    <Button>Create</Button>
+                </CardSection>
+
+            </Card>
         );
     }
 }
